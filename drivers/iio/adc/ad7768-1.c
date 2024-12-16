analogdevicesinc/linux

iio: adc: ad7768-1: add support for Synchronization over SPI
The synchronization method using GPIO requires the generated pulse to be
truly synchronous with the base MCLK signal. When it is not possible to
do that in hardware, the datasheet recommends using synchronization over
SPI, where the generated pulse is already synchronous with MCLK. This
requires the SYNC_OUT pin to be connected to SYNC_IN pin.

Add the option to handle device synchronization over SPI.

Signed-off-by: Jonathan Santos <Jonathan.Santos@analog.com>

drivers/iio/adc/ad7768-1.c
@@ -218,6 +218,8 @@ struct ad7768_state {
 	const char *labels[ARRAY_SIZE(ad7768_channels)];
 	struct gpio_desc *gpio_reset;
 	bool spi_is_dma_mapped;
+	bool en_spi_sync;
+	bool en_gpio_sync;
 	int irq;
 	/*
 	 * DMA (thus cache coherency maintenance) may require the
@@ -284,6 +286,19 @@ static int ad7768_spi_reg_write_masked(struct ad7768_state *st,
 		return ret;
 
 	return ad7768_spi_reg_write(st, addr, (reg_val & ~mask) | val);
+}
+
+static int ad7768_send_sync_pulse(struct ad7768_state *st)
+{
+	if (st->en_spi_sync)
+		return ad7768_spi_reg_write(st, AD7768_REG_SYNC_RESET, 0x00);
+
+	if (st->en_gpio_sync) {
+		gpiod_set_value_cansleep(st->gpio_sync_in, 1);
+		gpiod_set_value_cansleep(st->gpio_sync_in, 0);
+	}
+
+	return 0;
 }
 
 static int ad7768_set_mode(struct ad7768_state *st,
@@ -372,10 +387,7 @@ static int ad7768_set_dig_fil(struct ad7768_state *st,
 		return ret;
 
 	/* A sync-in pulse is required every time the filter dec rate changes */
-	gpiod_set_value(st->gpio_sync_in, 1);
-	gpiod_set_value(st->gpio_sync_in, 0);
-
-	return 0;
+	return ad7768_send_sync_pulse(st);
 }
 
 int ad7768_gpio_direction_input(struct gpio_chip *chip, unsigned int offset)
@@ -689,10 +701,17 @@ static int ad7768_setup(struct ad7768_state *st)
 	if (ret)
 		return ret;
 
+	st->en_gpio_sync = false;
+	st->en_spi_sync = false;
 	st->gpio_sync_in = devm_gpiod_get(&st->spi->dev, "adi,sync-in",
 					  GPIOD_OUT_LOW);
-	if (IS_ERR(st->gpio_sync_in))
-		return PTR_ERR(st->gpio_sync_in);
+	if (!IS_ERR(st->gpio_sync_in))
+		st->en_gpio_sync = true;
+
+	/* GPIO and SPI Synchronization are mutually exclusive */
+	if (!st->en_gpio_sync
+	    && device_property_present(&st->spi->dev, "adi,sync-in-spi"))
+		st->en_spi_sync = true;
 
 	ret = ad7768_gpio_init(st);
 	if (ret < 0)

philippdiethelm/adi-linux

drivers/iio/adc/ad7768-1.c
@@ -255,6 +255,7 @@ struct ad7768_state {
 	const char *labels[ARRAY_SIZE(ad7768_channels)];
 	struct gpio_desc *gpio_reset;
 	bool spi_is_dma_mapped;
+	bool en_spi_sync;
 	int irq;
 	/*
 	 * DMA (thus cache coherency maintenance) may require the
@@ -321,6 +322,19 @@ static int ad7768_spi_reg_write_masked(struct ad7768_state *st,
 		return ret;
 
 	return ad7768_spi_reg_write(st, addr, (reg_val & ~mask) | val);
+}
+
+static int ad7768_send_sync_pulse(struct ad7768_state *st)
+{
+	if (st->en_spi_sync)
+		return ad7768_spi_reg_write(st, AD7768_REG_SYNC_RESET, 0x00);
+
+	if (st->gpio_sync_in) {
+		gpiod_set_value_cansleep(st->gpio_sync_in, 1);
+		gpiod_set_value_cansleep(st->gpio_sync_in, 0);
+	}
+
+	return 0;
 }
 
 static int ad7768_set_mode(struct ad7768_state *st,
@@ -410,10 +424,7 @@ static int ad7768_set_dig_fil(struct ad7768_state *st,
 		return ret;
 
 	/* A sync-in pulse is required every time the filter dec rate changes */
-	gpiod_set_value(st->gpio_sync_in, 1);
-	gpiod_set_value(st->gpio_sync_in, 0);
-
-	return 0;
+	return ad7768_send_sync_pulse(st);
 }
 
 int ad7768_gpio_direction_input(struct gpio_chip *chip, unsigned int offset)
@@ -732,10 +743,20 @@ static int ad7768_setup(struct ad7768_state *st)
 	if (ret)
 		return ret;
 
-	st->gpio_sync_in = devm_gpiod_get(&st->spi->dev, "adi,sync-in",
+	st->gpio_sync_in = devm_gpiod_get_optional(&st->spi->dev, "adi,sync-in",
 					  GPIOD_OUT_LOW);
 	if (IS_ERR(st->gpio_sync_in))
 		return PTR_ERR(st->gpio_sync_in);
+
+	if (device_property_present(&st->spi->dev, "adi,sync-in-spi"))
+		st->en_spi_sync = true;
+
+	/*
+	 * GPIO and SPI Synchronization are mutually exclusive.
+	 * Return error if both are enabled
+	 */
+	if (st->gpio_sync_in && st->en_spi_sync)
+		return -EINVAL;
 
 	ret = ad7768_gpio_init(st);
 	if (ret < 0)